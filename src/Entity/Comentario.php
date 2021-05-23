283a9b401c814f7c23bbd5408e9608cc8a4af04d
AlvCarballo <alv.carballo@outlook.es>
2021-05-23 22:14:39 +0200

    Inicio de symfony y generacion de entidades y controladores

---
+++ src/Entity/Comentario.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Comentario
+ *
+ * @ORM\Table(name="comentario", indexes={@ORM\Index(name="uId_idx", columns={"coIdUsuarioFK"})})
+ * @ORM\Entity
+ */
+class Comentario
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="coId", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $coid;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="coTitle", type="string", length=45, nullable=true)
+     */
+    private $cotitle;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="coComentario", type="text", length=65535, nullable=true)
+     */
+    private $cocomentario;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="coPagina", type="string", length=45, nullable=true)
+     */
+    private $copagina;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="coCreated_at", type="datetime", nullable=true)
+     */
+    private $cocreatedAt;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="coDelete_at", type="datetime", nullable=true)
+     */
+    private $codeleteAt;
+
+    /**
+     * @var \User
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="coIdUsuarioFK", referencedColumnName="uId")
+     * })
+     */
+    private $coidusuariofk;
+
+    public function getCoid(): ?int
+    {
+        return $this->coid;
+    }
+
+    public function getCotitle(): ?string
+    {
+        return $this->cotitle;
+    }
+
+    public function setCotitle(?string $cotitle): self
+    {
+        $this->cotitle = $cotitle;
+
+        return $this;
+    }
+
+    public function getCocomentario(): ?string
+    {
+        return $this->cocomentario;
+    }
+
+    public function setCocomentario(?string $cocomentario): self
+    {
+        $this->cocomentario = $cocomentario;
+
+        return $this;
+    }
+
+    public function getCopagina(): ?string
+    {
+        return $this->copagina;
+    }
+
+    public function setCopagina(?string $copagina): self
+    {
+        $this->copagina = $copagina;
+
+        return $this;
+    }
+
+    public function getCocreatedAt(): ?\DateTimeInterface
+    {
+        return $this->cocreatedAt;
+    }
+
+    public function setCocreatedAt(?\DateTimeInterface $cocreatedAt): self
+    {
+        $this->cocreatedAt = $cocreatedAt;
+
+        return $this;
+    }
+
+    public function getCodeleteAt(): ?\DateTimeInterface
+    {
+        return $this->codeleteAt;
+    }
+
+    public function setCodeleteAt(?\DateTimeInterface $codeleteAt): self
+    {
+        $this->codeleteAt = $codeleteAt;
+
+        return $this;
+    }
+
+    public function getCoidusuariofk(): ?User
+    {
+        return $this->coidusuariofk;
+    }
+
+    public function setCoidusuariofk(?User $coidusuariofk): self
+    {
+        $this->coidusuariofk = $coidusuariofk;
+
+        return $this;
+    }
+
+
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="uId", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $uid;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="uEmail", type="string", length=45, nullable=true)
+     */
+    private $uemail;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="uPassword", type="string", length=255, nullable=true)
+     */
+    private $upassword;
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="uRole", type="integer", nullable=true)
+     */
+    private $urole;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="uNombre", type="string", length=45, nullable=true)
+     */
+    private $unombre;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="uApellidos", type="string", length=150, nullable=true)
+     */
+    private $uapellidos;
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="uTelefono", type="integer", nullable=true)
+     */
+    private $utelefono;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="uDireccion", type="string", length=45, nullable=true)
+     */
+    private $udireccion;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="uCreated_at", type="datetime", nullable=true)
+     */
+    private $ucreatedAt;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="uDelete_at", type="datetime", nullable=true)
+     */
+    private $udeleteAt;
+
+    public function getUid(): ?int
+    {
+        return $this->uid;
+    }
+
+    public function getUemail(): ?string
+    {
+        return $this->uemail;
+    }
+
+    public function setUemail(?string $uemail): self
+    {
+        $this->uemail = $uemail;
+
+        return $this;
+    }
+
+    public function getUpassword(): ?string
+    {
+        return $this->upassword;
+    }
+
+    public function setUpassword(?string $upassword): self
+    {
+        $this->upassword = $upassword;
+
+        return $this;
+    }
+
+    public function getUrole(): ?int
+    {
+        return $this->urole;
+    }
+
+    public function setUrole(?int $urole): self
+    {
+        $this->urole = $urole;
+
+        return $this;
+    }
+
+    public function getUnombre(): ?string
+    {
+        return $this->unombre;
+    }
+
+    public function setUnombre(?string $unombre): self
+    {
+        $this->unombre = $unombre;
+
+        return $this;
+    }
+
+    public function getUapellidos(): ?string
+    {
+        return $this->uapellidos;
+    }
+
+    public function setUapellidos(?string $uapellidos): self
+    {
+        $this->uapellidos = $uapellidos;
+
+        return $this;
+    }
+
+    public function getUtelefono(): ?int
+    {
+        return $this->utelefono;
+    }
+
+    public function setUtelefono(?int $utelefono): self
+    {
+        $this->utelefono = $utelefono;
+
+        return $this;
+    }
+
+    public function getUdireccion(): ?string
+    {
+        return $this->udireccion;
+    }
+
+    public function setUdireccion(?string $udireccion): self
+    {
+        $this->udireccion = $udireccion;
+
+        return $this;
+    }
+
+    public function getUcreatedAt(): ?\DateTimeInterface
+    {
+        return $this->ucreatedAt;
+    }
+
+    public function setUcreatedAt(?\DateTimeInterface $ucreatedAt): self
+    {
+        $this->ucreatedAt = $ucreatedAt;
+
+        return $this;
+    }
+
+    public function getUdeleteAt(): ?\DateTimeInterface
+    {
+        return $this->udeleteAt;
+    }
+
+    public function setUdeleteAt(?\DateTimeInterface $udeleteAt): self
+    {
+        $this->udeleteAt = $udeleteAt;
+
+        return $this;
+    }
+
+
+}
